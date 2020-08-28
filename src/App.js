7f293eb4d0c79872dcba0f8165730d2ee853b2b5
Saad Hassan <msaadhassan99@gmail.com>
2020-08-28 16:36:36 -0700

    facebook-clone

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import Sidebar from "./Sidebar"
+
+function App() {
+  return (
+    // BEM naming convention
+    <div className="app">
+
+      <Header/>
+
+      <div className="app_body">
+        <Sidebar/>
+        {/*SideBar*/} 
+        {/*Feed*/}
+        {/*Widgets*/}
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCVJGw5ECQiyyZMVPEpQrJtRCzvotvv4ok",
+    authDomain: "facebook-clone-59b30.firebaseapp.com",
+    databaseURL: "https://facebook-clone-59b30.firebaseio.com",
+    projectId: "facebook-clone-59b30",
+    storageBucket: "facebook-clone-59b30.appspot.com",
+    messagingSenderId: "662735957550",
+    appId: "1:662735957550:web:40b8aed9809ce3461c39bf",
+    measurementId: "G-W3N46B86XL"
+  };
